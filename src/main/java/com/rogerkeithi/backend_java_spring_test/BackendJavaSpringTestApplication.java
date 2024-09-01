192cf3af511e877d2c9a98f320c67acfc4c6d53e
Roger Keithi <nojiri.keithi@aluno.ifsp.edu.br>
2024-09-01 14:35:56 -0300

    feat: init project

---
+++ src/main/java/com/rogerkeithi/backend_java_spring_test/BackendJavaSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.rogerkeithi.backend_java_spring_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJavaSpringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogerkeithi/backend_java_spring_test/BackendJavaSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerkeithi.backend_java_spring_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
